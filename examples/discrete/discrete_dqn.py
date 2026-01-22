5fc8ba6df8e02f20373824717ef6b914a7a77743
yeungyf <stevenyangyfy@gmail.com>
2026-01-22 16:37:17 -0500

    Update discrete_dqn.py

--- examples/discrete/discrete_dqn.py
+++ examples/discrete/discrete_dqn.py
@@ -92,7 +92,7 @@ def main() -> None:
 
     # watch performance
     collector = ts.data.Collector[CollectStats](algorithm, env, exploration_noise=True)
-    collector.collect(n_episode=100, render=1 / 35)
+    collector.collect(n_episode=100, render=1 / 35, reset_before_collect=True)
 
 
 if __name__ == "__main__":
